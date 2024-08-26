fa2bb10f049777fc592bbf6db44f34da9e5ac622
Bioagecalculator <dz888@ukr.net>
2024-08-26 11:00:55 +0300

    Add files via upload

---
+++ bioage.pas
@@ -0,0 +1,15 @@
+program bioage;
+var a:integer;
+var b:integer;
+var c:integer;
+var r:integer;
+begin
+  writeln ('Bio age: e: zabiyakod@mail.ru');
+  writeln ('enter heart rate at rest count for 15 seconds and * 4');
+readln(a);
+writeln ('Age:');
+readln(b);
+r=(a/60)*b);
+writeln('bio age: year old ', r);
+end.
+
